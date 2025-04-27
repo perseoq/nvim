13dbd948e7351ee4d0e67eecf07bc8d5f0cb6750
Roy <rodrigoqyt@gmail.com>
2025-04-27 13:58:13 -0600

    Update nvim.sh

--- nvim.sh
+++ nvim.sh
@@ -42,8 +42,7 @@ mkdir -p ~/.config/nvim && \
 curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/perseoq/nvim/refs/heads/main/init.vim
 
 # instalando dependencias
-cd ~/.config/nvim/bundle/coc.nvim/
-npm i
+#cd ~/.config/nvim/bundle/coc.nvim/
 
 echo "¡Instalación completada!"
 
